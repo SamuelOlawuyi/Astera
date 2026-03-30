e2f192e84972ba1278486bad60c3c75e96bede37
sanmipaul <sanmipaul1@gmail.com>
2026-03-30 09:54:44 +0100

    Remove target/ from git tracking (now in .gitignore)

--- target/debug/deps/soroban_spec_rust-3a3f4d667c895f46.d
+++
@@ -1,9 +0,0 @@
-/home/bilal/bilal_projects/open-source/drips/Astera/target/debug/deps/soroban_spec_rust-3a3f4d667c895f46.d: /home/bilal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-rust-22.0.11/src/lib.rs /home/bilal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-rust-22.0.11/src/trait.rs /home/bilal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-rust-22.0.11/src/types.rs
-
-/home/bilal/bilal_projects/open-source/drips/Astera/target/debug/deps/libsoroban_spec_rust-3a3f4d667c895f46.rlib: /home/bilal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-rust-22.0.11/src/lib.rs /home/bilal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-rust-22.0.11/src/trait.rs /home/bilal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-rust-22.0.11/src/types.rs
-
-/home/bilal/bilal_projects/open-source/drips/Astera/target/debug/deps/libsoroban_spec_rust-3a3f4d667c895f46.rmeta: /home/bilal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-rust-22.0.11/src/lib.rs /home/bilal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-rust-22.0.11/src/trait.rs /home/bilal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-rust-22.0.11/src/types.rs
-
-/home/bilal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-rust-22.0.11/src/lib.rs:
-/home/bilal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-rust-22.0.11/src/trait.rs:
-/home/bilal/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/soroban-spec-rust-22.0.11/src/types.rs:
